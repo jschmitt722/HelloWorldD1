b5c04baa683d8b8bdd6d8655977e2267eca63c35
jds07 <jds07@LAPTOP-F33C9EO1>
2020-01-23 17:19:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorldD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pause display to Hello World!
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
